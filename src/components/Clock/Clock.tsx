2cb262b55453b026426146cf20397c754b20b354
Denys Raboshchuk <kontroler444@gmail.com>
2025-01-21 12:51:05 +0200

    Create React realtime Clock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+type State = {
+  currentTime: string;
+};
+
+interface Props {
+  name: string;
+}
+
+export class Clock extends React.Component<Props, State> {
+  state: State = {
+    currentTime: new Date().toUTCString().slice(-12, -4),
+  };
+
+  timerTimeId = 0;
+
+  componentDidMount(): void {
+    this.timerTimeId = window.setInterval(() => {
+      this.setState({
+        currentTime: `${new Date().toUTCString().slice(-12, -4)}`,
+      });
+      // eslint-disable-next-line no-console
+      console.log(this.state.currentTime);
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.name !== this.props.name) {
+      // eslint-disable-next-line no-console
+      console.warn(`Renamed from ${prevProps.name} to ${this.props.name}`);
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.timerTimeId);
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{this.props.name}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">{this.state.currentTime}</span>
+      </div>
+    );
+  }
+}
